ccfcd2dd7137f289ddf3befea2edbd37f1cdc9e7
abhinav-3202 <abhinavp3202@gmail.com>
2026-07-01 17:42:09 +0530

    email schema added

---
+++ src/models/EmailJob.js
@@ -0,0 +1,38 @@
+import mongoose,{Schema} from "mongoose";
+
+const emailJobSchema = new Schema({
+    to:{
+        type:String,
+        required:true,
+        lowercase:true,
+    },
+    subject:{
+        type:String,
+        required:true,
+    },
+    body:{
+        type:String,
+        required:true,
+    },
+    status:{
+        type:String,
+        enum:["pending", "sent", "failed"],
+        default:"pending",
+    },
+    attempts:{
+        type:Number,
+        default:0,
+    },
+    lastError:{
+        type:String,
+    },
+    idempotencyKey:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+},
+    {
+        timestamps:true,
+    },
+)
