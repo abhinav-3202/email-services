9f583627af5a66b26918453050d26b82b2b1b7f8
abhinav-3202 <abhinavp3202@gmail.com>
2026-07-07 12:45:55 +0530

    .env.smaple added

--- src/queue.js
+++ src/queue.js
@@ -3,7 +3,6 @@ import {Queue} from 'bullmq';
 const connection = {
     host: process.env.REDIS_HOST || 'localhost',
     port: process.env.REDIS_PORT || 6379,
-    // password: process.env.REDIS_PASSWORD || undefined,
 };
 
 export const emailQueue = new Queue('emailQueue', { connection });
